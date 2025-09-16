1ea9e6e84d14a2a3bbe25662525957c0648f1602
Jmartin59 <125609732+Obiwanjakobi5031@users.noreply.github.com>
2025-09-16 11:05:43 -0500

    Update GRID.py

--- GRID.py
+++ GRID.py
@@ -1,1 +1,12 @@
+from pydraw import
 
+screenMain = screen(1000, 800, 'GRID-OS')
+running = True
+print('start')
+print('')
+while running:
+question1 = input('--> ')
+if question1 == end:
+  screen.stop()
+else:
+  print('An error occured, please try again')
